fa6952f4d328c774142c39985aafe976d11aa305
Zhafirah <74231809+zhafirahy@users.noreply.github.com>
2021-08-27 09:41:40 +0700

    The Account Menu
    Source code one of the display menu, Account

---
+++ Account.java
@@ -0,0 +1,47 @@
+//Base class
+
+public abstract class Account{
+    private int accountNumber; 
+    // account number
+
+    protected double balance;
+    //balance
+
+    // Default constructor
+    public Account(){
+    }
+
+    public Account(int accountNumber){
+        this.accountNumber = accountNumber;
+        balance = 0;
+    }
+
+    //Getter methods
+    public double getBalance(){
+        return this.balance;
+    }
+
+    public int getAccountNumber(){
+        return this.accountNumber;
+    }
+
+    // Abstract
+    /**
+     * Function to deposit funds into the account as long as
+     * the amount parameter is >0
+     * 
+     * Apply transaction fee for the CheckingAccount 
+     * @param amount value to be deposited
+     */
+    public abstract void deposit(double amount);
+
+    /**
+     * Function to withdraw funds from the Account as long as
+     * 1. amount to withdraw must be >0
+     * 2. amount to withdraw must be = balance
+     * 
+     * @param amount value to be withdrawn
+     */
+    public abstract void withdraw(double amount);
+}
+
